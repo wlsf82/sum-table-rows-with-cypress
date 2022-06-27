a71f567da08a976e13301fef5e8dfb32de655cc7
Walmyr <wlsf82@gmail.com>
2022-06-28 00:43:39 +0200

    Change new test to new ones
    That better examplify what I want to show, besides showing
    different ways of solving the same problem.

--- cypress/e2e/other.cy.js
+++ cypress/e2e/other.cy.js
@@ -1,8 +1,35 @@
-describe('Table', () => {
-  it('finds an element based on a sub-group of elements', () => {
-    cy.visit('./index.html')
+describe("Students' messages", () => {
+  beforeEach(() => cy.visit('./other.html'))
 
-    cy.get('tbody tr:contains(5)')
-      .find('td:contains(Cappuccino)')
+  it('finds message 1 of student 1', () => {
+    cy.get('tr:contains(Message 1)')
+      .find('td:contains(Student 1)')
+      .parent()
+      .find('td')
+      .first()
+  })
+
+  it('finds message 2 of student 1', () => {
+    cy.get('tr:contains(Message 2)')
+      .find('td:contains(Student 1)')
+      .parent()
+      .children()
+      .first()
+  })
+
+  it('finds message 1 of student 2', () => {
+    cy.get('tr:contains(Message 1)')
+      .find('td:contains(Student 2)')
+      .parent()
+      .find('td')
+      .first()
+  })
+
+  it('finds message 2 of student 2', () => {
+    cy.get('tr:contains(Message 2)')
+      .find('td:contains(Student 2)')
+      .parent()
+      .children()
+      .eq(0)
   })
 })
